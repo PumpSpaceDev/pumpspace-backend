45fdc60f3b6373780b8db3379ebe842331769c43
PumpSpaceDev <evan@pump.space>
2024-12-27 14:26:20 +0800

    review libs/interfaces

--- libs/interfaces/src/entities/base.entity.ts
+++ libs/interfaces/src/entities/base.entity.ts
@@ -1,9 +1,13 @@
-import { CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 export abstract class BaseEntity {
   @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   created_at: Date;
 
-  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
   updated_at: Date;
 }
